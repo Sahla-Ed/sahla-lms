d7e3898fe544237978a44176bb672398ded3dd76
amr-ibrahim7 <amrnew100@gmail.com>
2025-07-13 23:14:48 +0300

    feat(ui): adding Courses pages

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -10,7 +10,7 @@ export default function LayoutPublic({ children }: { children: ReactNode }) {
       <Navbar />
       <main className="">
         {children}
-      </main>
+      </main>  
       <Footer />
     </div>
   );
