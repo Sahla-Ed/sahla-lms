a526a8cb6ac86176e28dba6b8c88be8f05e6e5ca
Mohamed Badran <mohamed.ahmed.badran0@gmail.com>
2025-07-13 18:34:27 +0300

    feat(admin): implement course editing and implement chapter and lesson CRUD

--- app/data/admin/admin-get-courses.ts
+++ app/data/admin/admin-get-courses.ts
@@ -4,8 +4,6 @@ import { prisma } from "@/lib/db";
 import { requireAdmin } from "./require-admin";
 
 export async function adminGetCourses() {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-
   await requireAdmin();
 
   const data = await prisma.course.findMany({
