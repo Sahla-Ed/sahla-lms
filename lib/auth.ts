36fa32ceeecf4a573d4378e46b33d16615d97589
mosaleh <mosaleh@mail.com>
2025-07-16 15:46:51 +0300

    feat(auth): introduce auth-ui route with github providers

--- lib/auth.ts
+++ lib/auth.ts
@@ -16,6 +16,33 @@ export const auth = betterAuth({
       clientSecret: env.AUTH_GITHUB_SECRET,
     },
   },
+  emailAndPassword: {
+    enabled: true,
+    sendResetPassword: async ({ user, url, token }, request) => {
+      console.log(user, token, url);
+      await resend.emails.send({
+        from: "sahla lms <onboarding@sahla.mosaleh.dev>",
+        to: [user.email],
+        subject: "Reset your password",
+        html: `<p>Click the link to reset your password: <a href="${url}">${url}</a></p>`,
+      });
+    },
+  },
+  emailVerification: {
+    sendOnSignUp: true,
+    autoSignInAfterVerification: true,
+    expiresIn: 3600,
+    sendVerificationEmail: async ({ user, url, token }, request) => {
+      //TODO: customize the email template with name and info
+      console.log(user, token, url);
+      await resend.emails.send({
+        from: "sahla lms <onboarding@sahla.mosaleh.dev>",
+        to: [user.email],
+        subject: "sahla lms - Verify your email",
+        html: `<p>Click the link to verify your email: <a href="${url}">${url}</a></p>`,
+      });
+    },
+  },
 
   plugins: [
     emailOTP({
@@ -23,7 +50,7 @@ export const auth = betterAuth({
         //TODO: customize the email template
         console.log(otp);
         await resend.emails.send({
-          from: "sahla lms <onboarding@resend.dev>",
+          from: "sahla lms <onboarding@sahla.mosaleh.dev>",
           to: [email],
           subject: "sahla lms - Verify your email",
           html: `<p>Your OTP is <strong>${otp}</strong></p>`,
