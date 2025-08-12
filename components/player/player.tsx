0f4275fbc2fc898fe01a5b48c7f7ada93a3c0d33
mosaleh <mosaleh@mail.com>
2025-08-12 16:26:24 +0300

    feat(player): add cover support

--- components/player/player.tsx
+++ components/player/player.tsx
@@ -1,14 +1,38 @@
+'use client';
 import '@vidstack/react/player/styles/base.css';
 import '@vidstack/react/player/styles/plyr/theme.css';
-import { MediaPlayer, MediaProvider } from '@vidstack/react';
+import { MediaPlayer, MediaProvider, Poster } from '@vidstack/react';
 import {
   PlyrLayout,
   plyrLayoutIcons,
 } from '@vidstack/react/player/layouts/plyr';
-export const Player = (props: { src: string }) => {
+export const Player = (props: {
+  src: string;
+  coverSrc?: string;
+  coverAlt?: string;
+}) => {
   return (
-    <MediaPlayer title='Sprite Fight' src={props.src}>
-      <MediaProvider />
+    <MediaPlayer
+      //WARN:this will hide all video cover's in the same page and we need to find a way to hide the cover only without introducing react state
+      onPlay={() => {
+        const coverElement = document.querySelector('.video-cover');
+        if (coverElement) {
+          coverElement.classList.add('hidden');
+        }
+      }}
+      title='Sprite Fight'
+      src={props.src}
+      playsInline={true}
+    >
+      <MediaProvider>
+        {props.coverSrc && (
+          <Poster
+            className='video-cover'
+            src={props.coverSrc}
+            alt={props.coverAlt}
+          />
+        )}
+      </MediaProvider>
       <PlyrLayout
         // thumbnails='https://files.vidstack.io/sprite-fight/thumbnails.vtt'
         icons={plyrLayoutIcons}
