ac7d84201267dc6dbf4416d1fb98bbfe2a123ee6
amr-ibrahim7 <amrnew10@gmail.com>
2025-09-02 12:38:38 +0300

    feat(payment): Implement localization and RTL support for payment pages

--- app/s/[subdomain]/payment/cancel/page.tsx
+++ app/s/[subdomain]/payment/cancel/page.tsx
@@ -1,9 +1,17 @@
 import { buttonVariants } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
-import { ArrowLeft, XIcon } from 'lucide-react';
+import { ArrowLeft, ArrowRight, XIcon } from 'lucide-react';
 import Link from 'next/link';
+import { getLocale, getTranslations } from 'next-intl/server';
+import { cn } from '@/lib/utils';
 
-export default function PaymentCancelled() {
+export default async function PaymentCancelled() {
+  const t = await getTranslations('PaymentPages.cancel');
+  const locale = await getLocale();
+  const isRTL = locale === 'ar';
+
+  const ArrowIcon = isRTL ? ArrowRight : ArrowLeft;
+
   return (
     <div className='flex min-h-screen w-full flex-1 items-center justify-center'>
       <Card className='w-[350px]'>
@@ -12,17 +20,17 @@ export default function PaymentCancelled() {
             <XIcon className='size-12 rounded-full bg-red-500/30 p-2 text-red-500' />
           </div>
           <div className='mt-3 w-full text-center sm:mt-5'>
-            <h2 className='text-xl font-semibold'>Payment Cancelled</h2>
+            <h2 className='text-xl font-semibold'>{t('title')}</h2>
             <p className='text-muted-foreground mt-2 text-sm tracking-tight text-balance'>
-              No worries, you wont be charged. Please try again!
+              {t('description')}
             </p>
 
             <Link
               href='/'
               className={buttonVariants({ className: 'mt-5 w-full' })}
             >
-              <ArrowLeft className='size-4' />
-              Go back to Homepage
+              <ArrowIcon className={cn('size-4', isRTL ? 'ml-2' : 'mr-2')} />
+              {t('button')}
             </Link>
           </div>
         </CardContent>
